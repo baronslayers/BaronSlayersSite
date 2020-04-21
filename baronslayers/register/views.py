01d15ea1cad804c29f458ae227cafc447bf114b2
Matheus Alefe Munari Zanardi <matheus.orvati@hotmail.com>
2020-04-21 14:28:23 -0300

    iniciar formulário de inscrição

--- baronslayers/register/views.py
+++ baronslayers/register/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def register(request):
-    return render(request, 'register.html')
+def register(response):
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
+    return render(response, 'register.html', {"form":form})
